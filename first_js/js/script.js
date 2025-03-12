8e34dcf76dc8c2e8fbd50dc478d85c76db772087
Zen <149407751+JosephZen@users.noreply.github.com>
2025-03-12 10:10:54 +0800

    Add files via upload

---
+++ first_js/js/script.js
@@ -0,0 +1,20 @@
+function sum(num1, num2){
+    const answer = num1 + num2;
+    console.log("The sum is ",answer);
+}
+
+function difference(num1, num2){
+    const answer = num1 - num2;
+    console.log("The difference is ",answer);
+}
+
+function quotient(num1, num2){
+    const answer = num1 / num2;
+    console.log("The quotient is ",answer);
+}
+
+function product(num1, num2){
+    const answer = num1 * num2;
+    console.log("The product is ",answer);
+}
+
